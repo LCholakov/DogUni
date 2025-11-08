7f86bd689d044119a5d1f7c9302864af146e15cd
LCholakov <l.cholakoff@gmail.com>
2025-11-08 19:20:15 +0200

    initial commit - running spring project with dependancies

---
+++ src/main/java/com/github/lcholakov/doguni/DogUniApplication.java
@@ -0,0 +1,13 @@
+package com.github.lcholakov.doguni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogUniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogUniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/lcholakov/doguni/DogUniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lcholakov.doguni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogUniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
