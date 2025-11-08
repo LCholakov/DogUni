ce95efc5e69874937c662b5c687e35450874863b
LCholakov <l.cholakoff@gmail.com>
2025-11-08 21:54:02 +0200

    feat: index page works. Looks kinda crap. Would prefer some dog themed background. Hopefully if there's time left. Probably won't have time left =[

---
+++ src/main/java/com/doguni/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.doguni.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .requestMatchers("/", "/register").permitAll()
+                .anyRequest().authenticated()
+            );
+
+        return httpSecurity.build();
+    }
+
+}
--- src/test/java/com/doguni/DogUniApplicationTests.java
+++ src/test/java/com/doguni/DogUniApplicationTests.java
@@ -1,4 +1,4 @@
-package com.github.lcholakov.doguni;
+package com.doguni;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
